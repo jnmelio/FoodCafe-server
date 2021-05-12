7c34d13dba83c4969117e4c211942b484c3c9d24
unknown <julienonnenmacher@hotmail.fr>
2021-05-12 17:53:45 +0200

    seed file modified

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,14 +3,14 @@ const { Schema, model } = require("mongoose");
 const recipeSchema = new Schema(
   {
     name: { type: String, required: true, unique: true },
-    ingredients: { type: String, required: true },             //???
+    ingredients: { type: [""], required: true },             //???
     instructions: { type: String, required: true },
-    youtube: String, password: { type: String, required: true },
+    youtube: String, 
     picture: String,
     description: { type: String, required: true },
     cookingTime: { type: Number, required: true },
     difficulty: String,
-    createdBy: { type: Schema.Types.ObjectId, ref: 'User' },
+    created_by: { type: Schema.Types.ObjectId, ref: 'User' },
     country: String,
     category: String,
     vegetarian: Boolean
