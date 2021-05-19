77afa6c9a30cf402a30ef490937571e404b76294
unknown <julienonnenmacher@hotmail.fr>
2021-05-19 15:06:14 +0200

    fixed some bugs

--- models/Post.model.js
+++ models/Post.model.js
@@ -1,5 +1,8 @@
 const { Schema, model } = require("mongoose");
 
+
+require('./User.model')
+require('./Recipe.model')
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const postSchema = new Schema(
   {
